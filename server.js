a6c94c7a7c8f53cdb5da4450253beade99320e9f
DiFrescoTisha-FS <81319127+DiFrescoTisha-FS@users.noreply.github.com>
2022-07-02 15:12:08 -0400

    Updates to Server

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use((req,res,next) => {
     error.status = 404;
     next(error);
 });
-
+ // middleware to handle changes nicely
 app.use((error, req, res, next) => {
     res.status(error.status || 500).json({
         error: {
