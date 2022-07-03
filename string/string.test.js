8bcf1b88473a4e2d35d60f7a8d814bd4c1848e70
DiFrescoTisha-FS <81319127+DiFrescoTisha-FS@users.noreply.github.com>
2022-07-03 14:21:59 -0400

    New test for the string module all passed

---
+++ string/string.test.js
@@ -0,0 +1,18 @@
+const { 
+    uppercase, 
+    lowercase
+ } =require("./string")
+
+describe("Testing the string module", () => {
+    test("should uppercase a string input", () => {
+
+        // expect(uppercase("bob")).toBe("BOB"); is the same as below 2 lines
+        const result = uppercase("bob");
+        expect(result).toBe("BOB");
+    });
+
+    test("should lowercase a string input", () => {
+        expect(lowercase("FULL SAIL")).toBe("full sail");
+    });
+});
+
